7248f01523e9b661180183f225e13581aa9c34f4
Dan Slaughter <danslaughter@Elios-MacBook-Pro.local>
2018-05-06 17:36:45 -0700

    updated and commented .py scripts

---
+++ final_project/annulus.py
@@ -0,0 +1,47 @@
+import matplotlib.pyplot as plt
+from itertools import cycle
+import numpy as np
+
+#create figure to display the active plot
+fig, ax = plt.subplots(figsize=(8, 8), facecolor='black')
+
+#create part of a grayscale which we can traverse through in different ways
+#move through grayscale linearly
+grays_linear = list(np.linspace(0.2, 0.8, 50))
+
+#move through grayscale cubicly
+grays_cubic = [x**3 for x in np.linspace(0.2,0.8,100)]
+
+#jump between the two extremes of grayscale (binary pattern/square wave)
+grays_binary = [0.2]*10 + [0.8]*10
+
+#create cycle iterator to return elements from the grayscale concatenated with its reversed self
+#assign desired pattern (linear, cubic, binary) below
+grays = cycle(grays_linear + list(reversed(grays_linear)))
+
+#create a linear space of values for the outer annulus radius which we can traverse through 
+sizes = list(np.linspace(0.2, 0.25, 50))
+
+#create cycle iterator to return elements from the list of radii concatenated with its reversed self
+sizes = cycle(sizes + list(reversed(sizes)))
+
+#create active figure using a loop
+while True:
+
+	#use __next__ method to retrieve next item (grayscale value) from cycle iterator
+    color = (next(grays), ) * 3 + (1., )
+
+	#use __next__ method to retrieve next item (outer radius) from cycle iterator
+    size = next(sizes)
+
+    #remove x-axis tick labels
+    ax.set_xticklabels([])
+
+	#plot annulus, pause for 0.01 seconds
+    circle1 = plt.Circle((0.5, 0.5), 0.15, color='black')
+    circle2 = plt.Circle((0.5, 0.5), size, color=color)
+    ax.add_artist(circle2)
+    ax.add_artist(circle1)
+    plt.axis('off')
+    plt.pause(0.01)
+    ax.clear()
--- final_project/moving_beam.py
+++ final_project/moving_beam.py
@@ -2,20 +2,45 @@ import matplotlib.pyplot as plt
 import numpy as np
 from itertools import cycle
 
+#create figure to display the active plot
 fig, ax = plt.subplots(figsize=(8, 8))
 
+#create 100 x 100 grid with values between 0 - 10
 x = np.linspace(0, 10, 100)
 y = np.linspace(0, 10, 100)
 x_, y_ = np.meshgrid(x, y)
 
+#define Gaussian standard deviation
 sigma = 0.5
-mu_ = [x for x in np.linspace(3, 7, 40)]
-mu_c = cycle(mu_ + list(reversed(mu_)))
 
-ax = plt.gca()
+#define a set of linear spaces of points to represent Gaussian mean
+#create cycle iterator to return elements from list of means concatenated with its reversed self 
+
+#mu_ = list(np.linspace(3, 7, 40))
+#mu_c = cycle(mu_ + list(reversed(mu_)))
+
+mu_1 = [[x, 3] for x in np.linspace(3, 7, 20)]
+mu_2 = [[7, y] for y in np.linspace(3, 7, 20)]
+mu_3 = [[x, 7] for x in reversed(np.linspace(3, 7, 20))]
+mu_4 = [[3, y] for y in reversed(np.linspace(3, 7, 20))]
+mu_ = mu_1 + mu_2 + mu_3 + mu_4
+mu_c = cycle(mu_)
+
+#create active figure using a loop
 while True:
+
+    #use __next__ method to retrieve next item (Gaussian mean) from cycle iterator
     mu = next(mu_c)
-    z = (1 / np.sqrt(2 * np.pi * sigma)) * np.exp(-((x_ - mu)**2 + (y_ - mu)**2) / (2 * sigma**2))
-    plt.imshow(z)
+
+    #generate 2D Gaussian
+    z = (1 / np.sqrt(2 * np.pi * sigma)) * np.exp(-((x_ - mu[0])**2 + (y_ - mu[1])**2) / (2 * sigma**2))
+
+    #remove tick labels
+    ax.set_xticklabels([])
+    ax.set_yticklabels([])
+
+    #plot 2D Gaussian, pause for 0.01 seconds
+    plt.imshow(z, cmap = 'inferno')
+    plt.axis('off')
     plt.pause(0.01)
     ax.clear()
